2001abc93b292fd538a8f32870040830669aa9d3
unicus-erik <54896526+unicus-erik@users.noreply.github.com>
2019-10-22 10:46:43 +0200

    Vroom vroom here comes the refactor tractor

--- src/main/java/unicus/spacegame/Sectormaps.java
+++ src/main/java/unicus/spacegame/Sectormaps.java
@@ -7,6 +7,8 @@ import java.util.Random;
 
 class Sectormaps extends JPanel {
     Random rand;
+    Random rft; //re-factor tractor
+    Random TriangleRandom;
     int starsize = 7;
     int ystart = 20;
     int yheight = 200;
@@ -19,6 +21,8 @@ class Sectormaps extends JPanel {
     int x_secs = 5;
     int y_secs = 4;
     int t_secs = x_secs * y_secs;
+    int[][][] secs_stars_coords = new int[t_secs][][]; //[n][m][] is {x,y,seed for Lars} to be implemented later.
+
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Map Frame");
@@ -27,12 +31,6 @@ class Sectormaps extends JPanel {
         frame.setSize(1050, 1050);
         frame.setVisible(true);
     }
-
-    public Sectormaps(Random r) {
-        rand = new Random(0);
-    }
-
-    //NOTE: If distance argument * number of stars is too large compared to sector size, stars may never fit and generator algo may loop forever.
 
     /** Calculates whether the hypotenuse between two points is shorter than the minimum distance argument.
      * Uses squares to save a square root call.
@@ -114,7 +112,6 @@ class Sectormaps extends JPanel {
             return foundLinearTriplet;
         }
     }
-
     /** Gets the closest pair of a star in sector1 and a star in sector2. Pythagorean distance.
      * Starts with first star in sector 1 and first star in sector 2 as 'default' result value
      * then iterates over all stars in each sector, calculating whether distance between those is shorter
@@ -145,12 +142,20 @@ class Sectormaps extends JPanel {
         }
         return result;
     }
-
+    /** Returns a point firmly inside the triangle coordinates given as argument. Complicated math is involved.
+     * Uses a slight buffer to avoid points being picked right on the edge.
+     * Buffer size is a function variable, not a parameter, for consistency.
+     * Buffer size is a fraction of triangle size.
+     *
+     * @param P1x First corner's x-coordinate
+     * @param P1y First corner's y-coordinate
+     * @param P2x Second corner's x-coordinate
+     * @param P2y Second corner's y-coordinate
+     * @param P3x Third corner's x-coordinate
+     * @param P3y Third corner's y-coordinate
+     * @return int[2] x and y coordinates of a point.
+     */
     public int[] pointInTriangle(int P1x, int P1y, int P2x, int P2y, int P3x, int P3y) {
-        /* Takes the triangle corner points P1,P2,P3 as arguments.
-         * Shifts inwards to create a buffer near zone edges.
-         * Randomly selects a point within the buffered inner triangle.
-         */
         double bf = 0.03; //buffer fraction, where 0.1 = 10%.
         int Q1x = (int) (P1x + (bf*((P2x-P1x)+(P3x-P1x)))); //New corner coordinates, moved bf of the way towards the others
         int Q1y = (int) (P1y + (bf*((P2y-P1y)+(P3y-P1y))));
@@ -159,14 +164,47 @@ class Sectormaps extends JPanel {
         int Q3x = (int) (P3x + (bf*((P1x-P3x)+(P2x-P3x))));
         int Q3y = (int) (P3y + (bf*((P1y-P3y)+(P2y-P3y))));
         //Now Q1,Q2,Q3 are the new corners
-        Random r = new Random();
-        double s = r.nextDouble();
-        double t = Math.sqrt(r.nextDouble()); //The sqrt counters biasing towards one corner that would result from wedge compression
-        double protox = (((1-t)*Q1x) + (t*(((1-s)*Q2x) + (s*Q3x)))); //Randomly weighted average of the coordinates of the corners
-        double protoy = (((1-t)*Q1y) + (t*(((1-s)*Q2y) + (s*Q3y)))); //Ditto
-        int x = (int) protox;
-        int y = (int) protoy;
+        double s = TriangleRandom.nextDouble();
+        double t = Math.sqrt(TriangleRandom.nextDouble()); //The sqrt counters biasing towards one corner that would otherwise result from wedge compression
+        double proto_x = (((1-t)*Q1x) + (t*(((1-s)*Q2x) + (s*Q3x)))); //Randomly weighted average of the coordinates of the corners
+        double proto_y = (((1-t)*Q1y) + (t*(((1-s)*Q2y) + (s*Q3y)))); //Ditto
+        int x = (int) proto_x;
+        int y = (int) proto_y;
         return new int[]{x,y};
+    }
+
+
+    public Sectormaps(Random r) {
+        rand = new Random(0);
+        rft = new Random(41356);
+        TriangleRandom = new Random(333);
+
+        //Create stars in secs_stars_coords[][][].
+        for (int i=0; i<x_secs; i++) {
+            for (int j=0; j<y_secs; j++) {
+                int ij = i+(j*x_secs); // linear number of sector, for array indexing
+                int[][] sc = new int[3][2]; //sc=sector_corners
+                int up = (i%2 == j%2) ? 1 : 0; //1 if triangle 'points' up, 0 if down.
+                int down = 1-up;
+                sc[0] = new int[]{xstart + (i * xmid), ystart + (yheight * (up+j))};
+                sc[1] = new int[]{(xstart + xmid + (i * xmid)), ystart + (yheight * (down + j))};
+                sc[2] = new int[]{xstart + xwidth + (i * xmid), ystart + (yheight * (up + j))};
+                int secstars_n = rft.nextInt(4)+1; //0-3 +1
+                int[][] secstars_c = new int[secstars_n][2];
+                /* NOTE: If number of stars (secstars_n) times minimum spacing (room) is too large compared to sector size,
+                 * stars may never fit and generator algorithm may loop forever.
+                 * assert ensures at least two stars fit in one sector, but is not a full guarantee of safety.
+                 * The general problem of circle-packing in a triangle is as yet unsolved. */
+                assert (room < xwidth || room < yheight );
+                do {
+                    for (int n = 0; n < secstars_n; n++) {
+                        int[] starcoords = pointInTriangle(sc[0][0], sc[0][1], sc[1][0], sc[1][1], sc[2][0], sc[2][1]);
+                        secstars_c[n] = starcoords.clone();
+                    }
+                } while (tooCloseInSector(secstars_c, room) || tooLinearInSector(secstars_c, angle));
+                secs_stars_coords[ij] = secstars_c.clone();
+            }
+        }
     }
 
     public void paintComponent(Graphics g) {
@@ -174,10 +212,51 @@ class Sectormaps extends JPanel {
         g.setColor(Color.black);
         g.fillRect(0,0,1000,1000);
 
+        //Draw hyperlanes
+        g.setColor(new Color(20,100,40));
+        //Within sectors
+        for (int i=1; i < secs_stars_coords.length; i++ ) {
+            int[][] localsector = secs_stars_coords[i];
+            if (localsector.length == 1) {
+                //Pass
+            } else if (localsector.length == 2) {
+                g.drawLine(localsector[0][0], localsector[0][1], localsector[1][0], localsector[1][1]);
+            } else if (localsector.length == 3) {
+                g.drawLine(localsector[0][0], localsector[0][1], localsector[1][0], localsector[1][1]);
+                g.drawLine(localsector[0][0], localsector[0][1], localsector[2][0], localsector[2][1]);
+                g.drawLine(localsector[1][0], localsector[1][1], localsector[2][0], localsector[2][1]);
+            } else if (localsector.length >= 4) {
+                int hub = rft.nextInt(localsector.length);
+                for (int v = 0; v < localsector.length; v++) {
+                    if (v != hub) {
+                        g.drawLine(localsector[v][0], localsector[v][1], localsector[hub][0], localsector[hub][1]);
+                    }
+                }
+            }
+        }
+        //Across sectors
+        for (int i = 1; i < secs_stars_coords.length; i++) {
+            if (i%x_secs != 0){ //do not wrap across end of line
+                int[][] crossSectorPair = getClosestPair(secs_stars_coords[i-1],secs_stars_coords[i]);
+                g.drawLine(crossSectorPair[0][0],crossSectorPair[0][1],crossSectorPair[1][0],crossSectorPair[1][1]);
+            }
+            int colrowsum = (i%x_secs)+(i/x_secs);
+            if (i >= x_secs && (colrowsum%2 == 1)) { //Roughly every 2nd sector should be linked to vertical above sector
+                int[][] crossSectorPair = getClosestPair(secs_stars_coords[i-x_secs],secs_stars_coords[i]);
+                g.drawLine(crossSectorPair[0][0],crossSectorPair[0][1],crossSectorPair[1][0],crossSectorPair[1][1]);
+            }
+        }
 
+        //Draw stars
+        g.setColor(new Color(200,200,90));
+        for (int i=0; i<secs_stars_coords.length; i++){
+            for (int j=0; j <secs_stars_coords[i].length; j++) {
+                g.fillOval(secs_stars_coords[i][j][0] - starsize / 2, secs_stars_coords[i][j][1] - starsize / 2, starsize, starsize);
+            }
+        }
+
+        //Draw sector grid
         g.setColor(new Color(240,60,140));
-
-        //Sector grid drawing
         for (int i=0; i<=y_secs; i++) { //draw horizontal sector lines
             if (i % 2 == 0) {
                 g.drawLine((xstart+xmid),(ystart+(i*yheight)), (xstart+(xmid*(x_secs+((x_secs+1)%2)))),(ystart+(i*yheight)));
@@ -200,63 +279,6 @@ class Sectormaps extends JPanel {
                         g.drawLine((xstart+xmid+(i*xmid)),(ystart+(j*yheight)), xstart+(i*xmid),(ystart+yheight+(j*yheight)));
                     }
                 }
-            }
-        }
-
-        int[][][] secs_stars_coords = new int[t_secs][][]; //[n][m][] is {x,y,seed for Lars} to be implemented later.
-        //Create stars
-        Random rft = new Random(41356);
-        for (int i=0; i<x_secs; i++) {
-            for (int j=0; j<y_secs; j++) {
-                int ij = i+(j*x_secs); // linear number of sector, for array indexing
-                System.out.println("Sector number "+ij);
-                int[][] sc = new int[3][2]; //sc=sector_corners
-                int up = (i%2 == j%2) ? 1 : 0; //1 if triangle 'points' up, 0 if down.
-                int down = 1-up;
-                sc[0] = new int[]{xstart + (i * xmid), ystart + (yheight * (up+j))};
-                sc[1] = new int[]{(xstart + xmid + (i * xmid)), ystart + (yheight * (down + j))};
-                sc[2] = new int[]{xstart + xwidth + (i * xmid), ystart + (yheight * (up + j))};
-                int secstars_n = rft.nextInt(4)+1; //0-3 +1
-                int[][] secstars_c = new int[secstars_n][2];
-                do {
-                    for (int n = 0; n < secstars_n; n++) {
-                        int[] starcoords = pointInTriangle(sc[0][0], sc[0][1], sc[1][0], sc[1][1], sc[2][0], sc[2][1]);
-                        secstars_c[n] = starcoords.clone();
-                    }
-                } while (tooCloseInSector(secstars_c, room) || tooLinearInSector(secstars_c, angle));
-
-                g.setColor(new Color(100,200,50));
-                for (int n=0; n<secstars_n; n++){
-                    g.fillOval(secstars_c[n][0] - starsize / 2, secstars_c[n][1] - starsize / 2, starsize, starsize);
-                }
-                secs_stars_coords[ij] = secstars_c.clone();
-                g.setColor(new Color(20,100,40));
-                if (secstars_n == 1) {
-                    assert true;
-                } else if (secstars_n == 2) {
-                    g.drawLine(secstars_c[0][0],secstars_c[0][1],secstars_c[1][0],secstars_c[1][1]);
-                } else if (secstars_n == 3) {
-                    g.drawLine(secstars_c[0][0],secstars_c[0][1],secstars_c[1][0],secstars_c[1][1]);
-                    g.drawLine(secstars_c[0][0],secstars_c[0][1],secstars_c[2][0],secstars_c[2][1]);
-                    g.drawLine(secstars_c[1][0],secstars_c[1][1],secstars_c[2][0],secstars_c[2][1]);
-                } else if (secstars_n >= 4) {
-                    int hub = rft.nextInt(secstars_n);
-                    for (int v=0; v<secstars_n; v++) {
-                        if (v != hub) { g.drawLine(secstars_c[v][0],secstars_c[v][1],secstars_c[hub][0],secstars_c[hub][1]); }
-                    }
-                }
-            }
-        }
-        //Create hyperlanes
-        for (int i = 1; i < secs_stars_coords.length; i++) {
-            if (i%x_secs != 0){ //do not wrap across end of line
-                int[][] crossSectorPair = getClosestPair(secs_stars_coords[i-1],secs_stars_coords[i]);
-                g.drawLine(crossSectorPair[0][0],crossSectorPair[0][1],crossSectorPair[1][0],crossSectorPair[1][1]);
-            }
-            int colrowsum = (i%x_secs)+(i/x_secs);
-            if (i >= x_secs && (colrowsum%2 == 1)) { //Roughly every 2nd sector should be linked to vertical above sector
-                int[][] crossSectorPair = getClosestPair(secs_stars_coords[i-x_secs],secs_stars_coords[i]);
-                g.drawLine(crossSectorPair[0][0],crossSectorPair[0][1],crossSectorPair[1][0],crossSectorPair[1][1]);
             }
         }
     }
