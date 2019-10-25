cf40e82e1290d59293ad293bf48467888ba9de79
Lars Erik Grambo <0kvakk@gmail.com>
2019-10-25 15:01:42 +0200

    Fix missing star connection bug
    Connections between stars for the first sub-sector were never calculated.
    Bug was not found earlier, as under development a fixed seed was used, and this always generated only 1 star in first sub-sector.

--- src/main/java/unicus/spacegame/Sectormaps.java
+++ src/main/java/unicus/spacegame/Sectormaps.java
@@ -351,7 +351,7 @@ public class Sectormaps extends JPanel implements Scrollable{
         //Draw hyperlanes
         g.setColor(new Color(20,100,40));
         //Within sectors
-        for (int i=1; i < secs_stars_coords.length; i++ ) {
+        for (int i=0; i < secs_stars_coords.length; i++ ) {
             int[][] localsector = secs_stars_coords[i];
             if (localsector.length == 1) {
                 //Pass
