26b8f18ec1f012b8fbac0d96a27af92499c4ce2e
unicus-erik <54896526+unicus-erik@users.noreply.github.com>
2019-10-25 14:45:11 +0200

    Hashmap add, remove working

--- src/main/java/unicus/spacegame/CargoBay.java
+++ src/main/java/unicus/spacegame/CargoBay.java
@@ -4,7 +4,7 @@ import java.awt.*;
 import java.util.HashMap;
 
 public class CargoBay {
-    final static int CARGO_MODULES_COUNT = 12;
+    private final static int CARGO_MODULES_COUNT = 12;
     CargoModule[] cargos;
     public static void main(String[] args) {
         //Create window
@@ -15,7 +15,7 @@ public class CargoBay {
         JPanel j_buttonpanel = new JPanel();
 
         //Create cargo
-        CargoBay allcargo = new CargoBay(j_statustextarea); //let printout know about textarea
+        CargoBay allcargo = new CargoBay(); //possibly: pass textarea argument to print out there?
         allcargo.createStartCargo();
 
         //Populate buttons
@@ -47,18 +47,27 @@ public class CargoBay {
         int remaining_amount = amount;
         for (CargoModule cargo : cargos) { //for each cargo module
             if (cargo.total_fullness < CargoModule.CARGO_CAPACITY_PER_MODULE) { //check if that module has space
-                int add_amount = Math.min(amount, CargoModule.CARGO_CAPACITY_PER_MODULE - cargo.total_fullness);
+                int add_amount = Math.min(remaining_amount, CargoModule.CARGO_CAPACITY_PER_MODULE - cargo.total_fullness);
                 remaining_amount -= add_amount;
                 cargo.AddCargo(add_amount, type);
             }
         }
+        if (remaining_amount != 0) { System.out.println("Couldn't fit it all.");}
         return (remaining_amount == 0);
     }
-    private void RemoveCargo(int amount, String type) {
-        int removedAmount = 0;
+    private boolean RemoveCargo(int amount, String type) {
+        int remaining_amount = amount;
         for (CargoModule cargo : cargos) {
-
+            int moduleAmount = cargo.contents.get(type);
+            if (moduleAmount > 0) {
+                int remove_amount = Math.min(remaining_amount, moduleAmount);
+                remaining_amount -= remove_amount;
+                cargo.total_fullness -= remove_amount;
+                cargo.contents.put(type, (cargo.contents.get(type)-remove_amount));
+            }
         }
+        if (remaining_amount != 0) { System.out.println("Didn't have that much.");}
+        return (remaining_amount == 0);
     }
 
     private void ListCargo() {
@@ -67,7 +76,7 @@ public class CargoBay {
         }
     }
 
-    public CargoBay(JTextArea text) {
+    public CargoBay() { //constructor
         cargos = new CargoModule[CARGO_MODULES_COUNT];
         for (int i=0; i<cargos.length; i++) {
             cargos[i] = new CargoModule(this, i);
@@ -83,7 +92,9 @@ public class CargoBay {
         }
     }
     public void createStartCargo() {
-        assert true;
+        AddCargo(12, "Fuel");
+        AddCargo(8, "Oxygen");
+        AddCargo( 2, "Water");
     }
 }
 
@@ -96,41 +107,37 @@ class CargoModule {
     private final static String CARGO_SPARES = "Spare parts";
     private final static String CARGO_SHINY = "Shinyium";
     private final static String[] CARGO_NAMES = {CARGO_FOOD, CARGO_WATER, CARGO_FUEL, CARGO_OXY, CARGO_SPARES, CARGO_SHINY};
-    final static int CARGO_TYPECOUNT = CARGO_NAMES.length;
+    //final static int CARGO_TYPECOUNT = CARGO_NAMES.length;
     final static int CARGO_CAPACITY_PER_MODULE = 60;
     int total_fullness;
-    int[] amount_per_cargo;
     String moduleID;
     HashMap<String, Integer> contents;
 
     public CargoModule(CargoBay cb, int id) { //constructor
         total_fullness = 0;
         contents = new HashMap<>();
-        amount_per_cargo = new int[CARGO_TYPECOUNT];
+        for (String s: CARGO_NAMES) {
+            contents.put(s, 0);
+        }
         CargoBay master = cb;
-        moduleID = (String) ("Module "+id);
+        moduleID = (String) ("Module "+(id+1));
     }
     public void AddCargo(int amount, String type) {
         if (total_fullness + amount > CARGO_CAPACITY_PER_MODULE) {
             //decide how to complain on capacity failure. return type bool?
         } else {
             total_fullness += amount;
-            //what's the equivalent of charAt or strPos?
-            for (int i=0; i<CARGO_TYPECOUNT; i++) {
-                if (type == CARGO_NAMES[i]) {
-                    amount_per_cargo[i] += amount;
-                }
-            }
+            contents.put(type, amount+contents.get(type));
         }
     }
 
     public void PrintCargo() {
         System.out.print(moduleID + " contains: ");
         boolean maybeEmpty = true;
-        for (int i=0; i<CARGO_TYPECOUNT; i++) {
-            if (amount_per_cargo[i] > 0) {
+        for (String s: CARGO_NAMES) {
+            if (contents.get(s) > 0) {
                 maybeEmpty = false;
-                System.out.print(amount_per_cargo[i] + " " +CARGO_NAMES[i] + ". ");
+                System.out.print(contents.get(s) + " " +s+ ". ");
             }
         }
         if (maybeEmpty) {  System.out.print("Nothing."); }
