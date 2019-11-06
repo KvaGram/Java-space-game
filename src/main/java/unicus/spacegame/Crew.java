e6a4fdc4dff242fa0760e11c701cffc7b1062339
unicus-erik <54896526+unicus-erik@users.noreply.github.com>
2019-11-06 14:04:19 +0100

    Update Crew.java

--- src/main/java/unicus/spacegame/Crew.java
+++ src/main/java/unicus/spacegame/Crew.java
@@ -43,11 +43,16 @@ public class Crew {
 }
 
 class Crewman {
+    static int ID_research = 0; //in order to refer to roles[ID]. maybe hashmap<string, boolean> instead?
+    static int ID_diplomacy = 1;
+    static int ID_medical = 2;
+    static int ID_teaching = 3;
     double age, stress, intelligence;
     boolean[] roles;
     int research, diplomacy, medical, teaching, navigation, engineering, mining, leadership, gunnery, boarding;
     String name;
     Crewman() {
+        this.name = makeSkiffyName();
         this.stress = 0;
         this.age = 20 + (new Random().nextInt(20)); //20-40
         this.intelligence = new Random().nextInt(100); //0-99
@@ -55,6 +60,20 @@ class Crewman {
     Crewman(String name) {
         this();
         this.name = name;
+    }
+    public String makeSkiffyName() {
+        String vowels = "aeiouy";
+        String consonants = "bcdfghjklmnprstvwxz";
+        char[] protoname = new char[6];
+        for (int i=0; i<protoname.length; i++) {
+            if (i%3 == 1) {
+                protoname[i] = vowels.charAt(new Random().nextInt(vowels.length()));
+            } else {
+                protoname[i] = consonants.charAt(new Random().nextInt(consonants.length()));
+            }
+            protoname[0] = Character.toUpperCase(protoname[0]);
+        }
+        return new String(protoname);
     }
     public void setRoles(boolean[] newRoles) {
         this.roles = newRoles;
