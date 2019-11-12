7343c8af37b52963b571ee2aa7c5a5eb8b8ced7e
Lars Erik Grambo <0kvakk@gmail.com>
2019-11-12 10:32:26 +0100

    fix test-render of asteroid fields
    There were several problems.
    For one, a mindless drooling stupid mistake were made when setting up the drawarc statement. It should be set based on the parent location, duh.
    For another, there were missing break statements in the switch-case for field shape.
    And more: There were integer divisions were there should be flaot divisions when calculating the mouseover-rect.
    aaand the rectangle had been scaled without being offset first.
    
    All fixed!

--- src/main/java/unicus/spacegame/ui/System/JunkFieldView.java
+++ src/main/java/unicus/spacegame/ui/System/JunkFieldView.java
@@ -1,6 +1,7 @@
 package unicus.spacegame.ui.System;
 
 import unicus.spacegame.structures.starsystem.BasicSpaceObject;
+import unicus.spacegame.structures.starsystem.JunkFieldShape;
 import unicus.spacegame.structures.starsystem.SpaceJunkField;
 
 import java.awt.*;
@@ -29,15 +30,19 @@ public class JunkFieldView extends SpaceObjectView {
             case BELT:
                 rotationMax = TAUf;
                 rotationMin = 0;
+                break;
             case CLUSTER:
                 rotationMax = object.getRadianLength()/2 + object.getOrbitRotation();
                 rotationMin = -object.getRadianLength()/2 + object.getOrbitRotation();
+                break;
             case CLOCKWISE_TAIL:
                 rotationMax = object.getRadianLength() + object.getOrbitRotation();
                 rotationMin = object.getOrbitRotation();
+                break;
             case COUNTER_CLOCKWISE_TAIL:
                 rotationMax = object.getOrbitRotation();
                 rotationMin = -object.getRadianLength() + object.getOrbitRotation();
+                break;
         }
         //wraparound rotation radian range
         while (rotationMax > TAUf)
@@ -45,30 +50,30 @@ public class JunkFieldView extends SpaceObjectView {
         while (rotationMin < 0)
             rotationMin += TAUf;
 
-        double fullDistanceSizeScaled = object.getOrbitDistance() + object.getLocalSize() * scale;
+        double fullDistanceSize = object.getOrbitDistance() + object.getLocalSize();
 
-        int highestX = (int)(Math.cos(rotationMax) * fullDistanceSizeScaled);
+        int highestX = (int)(Math.cos(rotationMax) * fullDistanceSize);
         int lowestX = highestX;
-        int highestY = (int)(Math.sin(rotationMax) * fullDistanceSizeScaled);
+        int highestY = (int)(Math.sin(rotationMax) * fullDistanceSize);
         int lowestY = highestY;
 
 
         boolean wraps0 = rotationMin > rotationMax;
         if(wraps0) //if the field arcs over the 0 radian
-            highestX = (int) fullDistanceSizeScaled;
-        if(rotationMax > (1/4)*TAUf && (wraps0 || rotationMin < (1/4)*TAUf) )
-            highestY = (int) fullDistanceSizeScaled;
-        if(rotationMax > (2/4)*TAUf && (wraps0 || rotationMin < (1/4)*TAUf) )
-            lowestX = (int) fullDistanceSizeScaled;
-        if(rotationMax > (3/4)*TAUf && (wraps0 || rotationMin < (1/4)*TAUf) )
-            lowestY = (int) fullDistanceSizeScaled;
+            highestX = (int) fullDistanceSize;
+        if(rotationMax > (1f/4)*TAUf && (wraps0 || rotationMin < (1f/4)*TAUf) )
+            highestY = (int) fullDistanceSize;
+        if(rotationMax > (2f/4)*TAUf && (wraps0 || rotationMin < (1f/4)*TAUf) )
+            lowestX = (int) -fullDistanceSize;
+        if(rotationMax > (3f/4)*TAUf && (wraps0 || rotationMin < (1f/4)*TAUf) )
+            lowestY = (int) -fullDistanceSize;
 
-        rect.x = lowestX;
-        rect.width = highestX - lowestX;
-        rect.y = lowestY;
-        rect.width = highestY - lowestY;
+        rect.width = (int) ((highestX - lowestX)*scale);
+        rect.height = (int) ((highestY - lowestY)*scale);
+        rect.x = (int)((lowestX + offset.x)*scale);
+        rect.y = (int)((lowestY + offset.y)*scale);
 
-        int strokeWidth = (int) Math.max(object.getLocalSize() * scale, 10);
+        int strokeWidth = (int) Math.max(object.getLocalSize() * scale, 6);
         drawStroke = new BasicStroke(strokeWidth,BasicStroke.CAP_ROUND,BasicStroke.JOIN_ROUND,10);
     }
 
@@ -107,12 +112,23 @@ public class JunkFieldView extends SpaceObjectView {
     @Override
     public void paintObject(Graphics2D g) {
         //super.paintObject(g);
-
-        int startAngle = (int) Math.toDegrees(rotationMin);
-        int arcAngle   = (int) Math.toDegrees(Math.abs(rotationMax - rotationMin));
         g.setStroke(drawStroke);
-        g.setColor(planetTint);
-        g.drawArc(rect.x, rect.y, rect.width, rect.height, startAngle, arcAngle);
+        //g.setColor(planetTint);
+        g.setColor(Color.white);
+        if(object.getFieldShape() == JunkFieldShape.BELT) {
+            g.drawOval(pLocation.x-orbitRad, pLocation.y-orbitRad, orbitRad*2, orbitRad*2);
+        } else {
+            int startAngle = (int) Math.toDegrees(rotationMin);
+            int arcAngle   = (int) Math.toDegrees(Math.abs(rotationMax - rotationMin));
+            g.drawArc(pLocation.x-orbitRad, pLocation.y-orbitRad, orbitRad*2, orbitRad*2, startAngle, arcAngle);
+        }
         g.setStroke(new BasicStroke(1));
+    }
+
+    @Override
+    public void paintObjectOrbit(Graphics2D g) {
+        //g.setColor(Color.PINK);
+        g.setColor(orbitColor);
+        g.drawOval(pLocation.x-orbitRad, pLocation.y-orbitRad, orbitRad*2, orbitRad*2);
     }
 }
