49256c86e737d93081967ebf4314930b60e136d0
Lars Erik Grambo <0kvakk@gmail.com>
2019-11-28 12:22:10 +0100

    Play with helloworld
    Just keeping what I have learned as refrence

--- src/main/java/enginetest/HelloWorld.java
+++ src/main/java/enginetest/HelloWorld.java
@@ -2,22 +2,28 @@ package enginetest;
 
 import de.gurkenlabs.litiengine.Game;
 import de.gurkenlabs.litiengine.GameListener;
+import de.gurkenlabs.litiengine.graphics.RenderComponent;
 import de.gurkenlabs.litiengine.graphics.TextRenderer;
 import de.gurkenlabs.litiengine.gui.screens.GameScreen;
 import de.gurkenlabs.litiengine.gui.screens.Screen;
+import de.gurkenlabs.litiengine.input.Input;
 import de.gurkenlabs.litiengine.resources.Resources;
 
+import javax.imageio.ImageIO;
+import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class HelloWorld {
     public static void main(String[] args) {
         HelloWorld hello = new HelloWorld();
 
-        System.out.println("main - before init");
+//        System.out.println("main - before init");
         hello.init();
-        System.out.println("main - before run");
+//        System.out.println("main - before run");
         hello.run();
-        System.out.println("main - end of main");
+//        System.out.println("main - end of main");
     }
 
     private void init() {
@@ -42,7 +48,26 @@ public class HelloWorld {
                 System.out.println("Hello event terminated");
             }
         });
+        Game.setInfo("gameinfo.xml");
+
         Game.init();
+        Image cursor;
+        try {
+
+            //try loading file.
+            cursor = ImageIO.read(Resources.getLocation("cursor1.png")).getScaledInstance(16, 16, Image.SCALE_SMOOTH);
+        } catch (IOException | IllegalArgumentException err) {
+            //paint backup icon.
+            System.out.println(err);
+            cursor = new BufferedImage(16, 16, BufferedImage.TYPE_INT_RGB);
+            Graphics g = cursor.getGraphics();
+            g.setColor(Color.red);
+            g.drawOval(0, 0, 16, 16);
+        }
+        Game.window().getRenderComponent().setCursor(cursor);
+        //Input.mouse().setGrabMouse(false);
+
+
         TestScreen testScreen = new TestScreen("TEST");
         Game.screens().add(testScreen);
 
@@ -62,11 +87,12 @@ public class HelloWorld {
 class TestScreen extends GameScreen {
     protected TestScreen(String screenId) {
         super(screenId);
+
     }
     @Override
     public void render(final Graphics2D g) {
         super.render(g);
-        g.setFont(Resources.fonts().get("customfont.ttf",32f));
+        //g.setFont(Resources.fonts().get("customfont.ttf",32f));
         g.setColor(Color.RED);
         TextRenderer.render(g, "Test text", 100, 100);
     }
