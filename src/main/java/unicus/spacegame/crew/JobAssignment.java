6e1f8c15959b82465bb8540e54240cc1722ce34e
Lars Erik Grambo <0kvakk@gmail.com>
2020-01-13 14:48:53 +0100

    Make  JobAssignment temporary store workload and work done per month
    This is used as a communication between Job and Crewman.
    Workload impacts the crewman morale, and the worl done impacts a job's operation.

--- src/main/java/unicus/spacegame/crew/JobAssignment.java
+++ src/main/java/unicus/spacegame/crew/JobAssignment.java
@@ -5,11 +5,18 @@ public class JobAssignment {
     private final int crewID;
     private WorkShare workshare;
 
+    //temporary stored values.
+    private double monthWorkload;
+    private double monthWork;
+
     public JobAssignment(int jobID, int crewID){
 
         this.jobID = jobID;
         this.crewID = crewID;
         this.workshare = WorkShare.full;
+
+        this.monthWorkload = 0;
+        this.monthWork = 0;
     }
 
     public WorkShare getWorkshare() {
@@ -27,22 +34,69 @@ public class JobAssignment {
     public int getJobID() {
         return jobID;
     }
+
+    /**
+     * Gets the amount of workload shared for this assignment.
+     * @return
+     */
+    public double getWorkload(){
+        int totalShares = 0;
+        for (JobAssignment ja:SpaceCrew.getInstance().getJobAssignmentsByJob(jobID)) {
+            totalShares += ja.workshare.share;
+        }
+        double WorkLoadShare = (double)workshare.share / (double)totalShares;
+        AbstractJob j = SpaceCrew.getInstance().getJob(jobID);
+        return WorkLoadShare * j.getMonthlyWorkload();
+    }
+
+    /**
+     * The amount of work done by this assignment by the end of the month.
+     * To be called only by {@link AbstractJob#endOfMonth() }
+     * @return
+     */
+    public double getMonthWork() {
+        return monthWork;
+    }
+
+    public void setMonthWork(double monthWork) {
+        this.monthWork = monthWork;
+    }
+
+    /**
+     * Amount of workload from this assignment this month. This impacts a crewman's crewman's morale.
+     * @return
+     */
+    public double getMonthWorkload() {
+        return monthWorkload;
+    }
+
+    public void setMonthWorkload(double monthWorkload) {
+        this.monthWorkload = monthWorkload;
+    }
+
 }
 enum WorkShare {
     /**
      * This crewmember has taken a vacation from this job, and gets no share of the workload.
     */
-    vacation,
+    vacation(0),
     /**
      * This crewmember has a reduced share of the workload
      */
-    reduced,
+    reduced(1),
     /**
      * This crewmember has a normal share of the workload.
      */
-    full,
+    full(3),
     /**
      * This crewmember has been asked to take up the slack, and takes on an extra share of the workload
      */
-    extended
+    extended(5);
+
+    public final int share;
+
+    WorkShare(int share) {
+
+        this.share = share;
+    }
 }
