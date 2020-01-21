a051117fc0e9155fc3be0e6ff7a03fae01514f69
Lars Erik Grambo <0kvakk@gmail.com>
2020-01-21 14:22:45 +0100

    Fix mistakes in work calculation in WorkAssignment
    A little programmar woopsie resulted in the wrong function calls.

--- src/main/java/unicus/spacegame/crew/JobAssignment.java
+++ src/main/java/unicus/spacegame/crew/JobAssignment.java
@@ -31,7 +31,7 @@ public class JobAssignment {
      */
     public void endOfMonth() {
         monthWorkloadShare = getWorkloadShare();
-        monthWorkProduced = getMonthWorkProduced();
+        monthWorkProduced = calculateWork();
         //TODO: chance to trigger workplace event
     }
 
@@ -40,8 +40,10 @@ public class JobAssignment {
      * @return An amount of work that will be produced by this crewman on this assignment.
      */
     public double calculateWork() {
-        //Get modifier
+        //Get modifier - crewman efficiency on job. factors in skill, traits, etc.
+        //NOTE: getWorkModifierOfCrewman may or may not call AdultCrewman#getGeneralWorkModifier(), depending on implementation.
         double m = SpaceCrew.getInstance().getJob(jobID).getWorkModifierOfCrewman(crewID);
+        //multiplies with the crewman's share in the job
         return m * getWorkloadShare();
     }
 
@@ -54,8 +56,10 @@ public class JobAssignment {
      * @return A base value amount of work, used for calculating production and morale impact.
      */
     public double getWorkloadShare() {
+        // Get how much work is expected or required on the job
         double w = SpaceCrew.getInstance().getJob(jobID).getMonthlyWorkload();
-        return w * getWorkloadShare();
+        // Get a share of how much of this work is on the assigned crewman
+        return w * getWorkloadShareModifier();
     }
 
     /**
@@ -64,10 +68,11 @@ public class JobAssignment {
      * @return A modifier value between 0.0 and 1.0
      */
     public double getWorkloadShareModifier() {
-        int totalShares = 0;
+        int totalShares = 0; //total 'shares' of the job.
         for (JobAssignment ja:SpaceCrew.getInstance().getJobAssignmentsByJob(jobID)) {
             totalShares += ja.workshare.share;
         }
+        //Divide the crewman's share with the total amount to get their share of the workload.
         return (double)workshare.share / (double)totalShares;
     }
 
